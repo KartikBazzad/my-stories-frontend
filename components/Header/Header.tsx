ec374b6c05ba7967e0e510270178fe80b4d8b701
KartikBazzad <kartikbazzad@gmail.com>
2022-02-13 17:34:00 +0530

    Fixed Write button in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -44,27 +44,25 @@ function Header() {
             <Link href={"/"}>Home</Link>
           </span>
           {user.isLoggedIn && (
-            <span className="button button-primary">
-              <Link passHref href={"/stories/create"}>
-                <span className="flex gap-1 cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                    />
-                  </svg>
-                  Write
-                </span>
-              </Link>
-            </span>
+            <Link passHref href={"/stories/create"}>
+              <button className="button button-primary flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                  />
+                </svg>
+                Write
+              </button>
+            </Link>
           )}
         </div>
         {!user.isLoggedIn && (
